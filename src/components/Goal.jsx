cc0d2dce3f18056ab9ffd445851af56f789c23f8
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-12 12:10:58 -0800

    goal with destination

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom'
 const Goal = ({goal}) => {
 
     return (
-        <div className='location-grid'>
+        <div className='destination-grid'>
             <h1>
                 Dream Destinations:
             </h1>
-            {}
-            <div>
-
-            </div>
+            {goal.map(destination => (
+                    <div className='destination-card' key={destination.id}>
+                    <Link to={`${destination._id}`}>
+                    <h2>{destination.location}</h2>
+                    <h3>{destination.description}</h3>
+                    <img className="destination-image" src={destination.image} alt={destination.location} />
+                    </Link>
+                    </div>
+                ))}
         </div>
     )
 }
