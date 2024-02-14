0828ef124ec0b82acecd2ec77fbc7d93a77ef0dd
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-14 08:19:27 -0800

    journal edit and delete

--- src/components/ArrivedDetail.jsx
+++ src/components/ArrivedDetail.jsx
@@ -6,6 +6,8 @@ import JournalForm from "./JournalForm";
 const ArrivedDetail = ({arrived}) => {
     const [arrive, setArrive] = useState(null);
     const [journal, setJournal] = useState([]);
+    const [editMode, setEditMode] = useState(false);
+    const [editedJournal, setEditedJournal] = useState({ date: "", diary: ""});
     let {id} = useParams()
 
     const getArrive = async () => {
@@ -38,7 +40,30 @@ const ArrivedDetail = ({arrived}) => {
     };
 
     const handleEdit = (journalId) => {
-        history.push(`/edit-journal/${journalId}`)
+        setEditMode(!editMode)
+        if (editMode) {
+            const selectedJournal = journal.arrivedJournal.find(
+                (journal => journal._id === journalId);
+        setEditedJournal({
+            date: selectedJournal.date,
+            diary: selectedJournal.diary,
+        })
+        } else {
+            setEditedJournal({ date: "", diary: ""});
+        }
+    };
+
+    const handleSave = async (journalId) => {
+        try {
+            await Client.put(`/journal/${journalId}`, {
+                date: editedJournal.date,
+                diary: editedJournal.diary,
+            })
+            setEditMode(false)
+            getJournal()
+        } catch (error) {
+            console.error("Error updating journal entry", error)
+        }
     }
 
     return arrive ? (
@@ -47,16 +72,46 @@ const ArrivedDetail = ({arrived}) => {
             <div key={arrive.id} className='location-card'>
             <h2>{arrive.location}</h2>
             <h3>{arrive.date}</h3>
-            <img className="location-image" src={arrive.picture} alt={arrive.location} />
+            <img className="location-image" 
+            src={arrive.picture} 
+            alt={arrive.location} />
             <br />
             <JournalForm getJournal={getJournal}/>
-            {journal.arrivedJournal.map(journal =>(
+            {journal.arrivedJournal.map((journal) =>(
                 <div key={journal.id}>
                     <img className="journal-image" src={journal.picture}/>
                     <p>{journal.date}</p>
+                    {editMode ? (
+                        <>
+                        <input
+                        type="text"
+                        value={editedJournal.date}
+                        onChange={(e) =>
+                        setEditedJournal({ ...editedJournal, date: e.target.value})
+                    }
+                    />
+                    <input 
+                    type="text" 
+                    value={editedJournal.diary}
+                    onChange={(e) =>
+                    setEditedJournal ({ ...editedJournal, diary: e.target.value})
+                    }
+                    />
+                    </>
+                    ) : (
                     <p>{journal.diary}</p>
-                    <button onClick={() => handleEdit(journal.id)}>Edit</button>
-                    <button onClick={() => handleDelete(journal.id)}>Delete</button>
+                    )}
+                    {editMode ? (
+                        <>
+                        <button onClick={() => handleSave(journal.id)}>Save</button>
+                        <button onClick={() => handleEdit(journal.id)}>Cancel</button>
+                        </>
+                    ) : (
+                        <>
+                        <button onClick={() => handleEdit(journal.id)}>Edit</button>
+                        <button onClick={() => handleDelete(journal.id)}>Delete</button>
+                        </>
+                    )}
                 </div>
             ))}
             </div>
