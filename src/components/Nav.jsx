1e96f4aa8fb66376a75558dc349d396d665109b6
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-15 08:28:12 -0800

    working nav bar update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,46 +2,70 @@ import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
 
 const Nav = ({ user, handleLogOut }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+    const closeMenu = () => {
+      setMenuOpen(false);
+    };
+
+    const handleLogOutAndCloseMenu = () => {
+      handleLogOut();
+      closeMenu();
+    };
+
   let userOptions
   if (user) {
     userOptions = (
-        <div>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink onClick={handleLogOut} to="/">
-          Sign Out
-        </NavLink>
-        <NavLink to="/arrived">
+      <>
+        <NavLink to="/" className="nav-link" 
+        onClick={closeMenu}>
+          Home
+          </NavLink>
+        <NavLink to="/arrived" className="nav-link"
+         onClick={closeMenu}>
                 <h4>Explored Destinations </h4>
             </NavLink>
-            <NavLink to="/goal">
+            <NavLink to="/goal" className="nav-link" 
+            onClick={closeMenu}>
                 <h4>Dream Destinations </h4>
             </NavLink>
-      </nav>
-      </div>
+        <NavLink to="/" className="nav-link" 
+        onClick={handleLogOutAndCloseMenu}>
+          Sign Out
+        </NavLink>
+      </>
     )
   }
 
   const publicOptions = (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/signin">Sign In</NavLink>
-    </nav>
+    <>
+      <NavLink to="/" className="nav-link"
+      onClick={closeMenu}>Home</NavLink>
+      <NavLink to="/register" className="nav-link"
+      onClick={closeMenu}>Register</NavLink>
+      <NavLink to="/signin" className="nav-link"
+      onClick={closeMenu}>Sign In</NavLink>
+    </>
   )
 
   return (
     <header>
-      <NavLink to="/">
-        <div className="logo-wrapper" alt="logo">
-          <img
-
-          />
-        </div>
-      </NavLink>
-      {user ? userOptions : publicOptions}
+      <nav className='navigation'>
+      <div className={`menu-toggle ${menuOpen ? 'open' : ''}`}
+      onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+        {user && userOptions}
+        {!user && publicOptions}
+      </div>
+      </nav>
     </header>
   )
 }
