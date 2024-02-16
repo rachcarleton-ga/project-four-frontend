b1bef22088d258877099bfa8bacebbc3df2aba1b
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-15 18:46:08 -0800

    base url update

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 import { config } from "dotenv"
 
-export const BASE_URL="http://localhost:4000/"
-
-// "https://destination-wanderlust-00e92fff4f50.herokuapp.com/"
+export const BASE_URL="https://destination-wanderlust-00e92fff4f50.herokuapp.com/"
 
 const Client = axios.create({baseURL: BASE_URL})
 
